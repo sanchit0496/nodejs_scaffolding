4ae917e72b6bdf6e7de0a085939a3aa9f8fc228e
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-14 00:27:28 +0530

    user: setting alter as true

--- users/index.js
+++ users/index.js
@@ -3,8 +3,15 @@ const sequelize = require('./config/database');
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+
+sequelize.sync({ alter: true }) // This will check what is the current state of the table in the database (which columns it has, what are their data types, etc), and then perform the necessary changes in the table to make it match the model.
+    .then(() => {
+        console.log('Database synced');
+        // ... start your server here
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error syncing database:', err);
     });
-});
