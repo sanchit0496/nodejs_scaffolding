857d547cdaf6f8483939080b7a4394703d37c947
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-14 14:45:44 +0530

    added swagger on user

--- users/app.js
+++ users/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const logger = require("./logger"); // Make sure the path is correct
 const userRoutes = require('./routes/userRoutes');
+const { swaggerSpec, swaggerUi } = require('./swaggerConfig'); // Adjust the path if necessary
 
 app.use(express.json()); // For parsing application/json
+
+// Swagger route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req, res, next) => {    
     // Log the request method and URL
--- users/routes/userRoutes.js
+++ users/routes/userRoutes.js
@@ -2,6 +2,45 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *       example:
+ *         id: d5fE_asz
+ *         name: John Doe
+ *         email: johndoe@example.com
+ * 
+ * /users:
+ *   get:
+ *     summary: Returns a list of users
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
 // Define the CRUD routes for users
 router.post('/', userController.createUser);
 router.get('/:id', userController.getUserById);
